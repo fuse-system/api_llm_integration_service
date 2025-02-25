333c30fb3774c3b30594f4f4942d233eee68ecfc
mahmoudnabil133 <hodanabil144@gmail.com>
2025-02-25 22:00:18 +0200

    update

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -90,7 +90,6 @@ export class AppController {
   @MessagePattern('call-llm')
   async sendHabbit(data: { message: string; llmType: string }) {
     let answer;
-    console.log(data);
     const messages: Array<{
       role: 'user' | 'assistant' | 'system';
       content: string;
